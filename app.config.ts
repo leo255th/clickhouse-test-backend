71c49742ebdbf6563f13b3b4b7ab1be65a20196e
yixiongyu <leoneyi@qq.com>
2021-12-09 18:03:47 +0800

    tmp

--- app.config.ts
+++ app.config.ts
@@ -12,7 +12,7 @@ export const AppConfig = {
       session_timeout: 60,
       output_format_json_quote_64bit_integers: 0,
       enable_http_compression: 0,
-      database: 'data_test_default',
+      database: 'data_test_stream',
     },
   }
 }
