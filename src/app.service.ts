d8b115b2a820ed60cee1ab7249f36a418a573835
yixiongyu <leoneyi@qq.com>
2021-12-15 01:06:32 +0800

    tmp

--- src/app.service.ts
+++ src/app.service.ts
@@ -146,7 +146,7 @@ export class AppService {
   }
   randomValueArray(): Array<number> {
     // 最开始的数据是ID字段，都写1
-    const a = [];
+    const a = [1];
     for (let i = 1; i <= 300; i++) {
       a.push(Math.round(Math.random()));
 
